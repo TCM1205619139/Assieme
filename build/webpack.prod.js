9e858fd6acc9d00e98c2676dab76b19b1a1804a6
TCM1205619139 <1205619139@qq.com>
2021-07-12 11:32:43 +0800

    webpack config

---
+++ build/webpack.prod.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const webpackBaseConfig = require('./webpack.base.js');
+
+module.exports = merge(webpackBaseConfig, {
+  mode: "production",
+  entry: path.resolve(__dirname, "../index.js"),
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/dist",
+    filename: "index.js",
+    library: "assieme",
+    libraryTarget: "umd",
+    umdNamedDefine: true
+  },
+  externals: {
+    vue: {
+      root: "Vue",
+      commonjs: 'vue',
+      commonjs2: 'vue',
+      amd: 'vue'
+    }
+  }
+})
